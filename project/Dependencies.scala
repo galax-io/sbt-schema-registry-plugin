f08127fb2447de31392a0b0d435043c9085f5738
John <jigarkhwar88@gmail.com>
2026-05-31 09:58:56 -0300

    feat: add integration tests using Testcontainers (real Schema Registry + Kafka) (#14)
    * feat: add WireMock dependency for integration tests
    
    * feat: add WireMock to test dependencies
    
    * feat: add WireMock-based integration tests for Downloader
    
    * feat: replace WireMock with Testcontainers deps for real integration tests
    
    * feat: add IntegrationTest config, remove WireMock dependency
    
    * feat: remove WireMock-based integration test
    
    * feat: add Testcontainers-based integration tests with real Schema Registry + Kafka
    
    * fix: correct container networking, add readiness probe, cleanup, stronger assertions
    
    * fix: remove unused testcontainers-scala-scalatest dep
    
    * fix: remove unused testcontainers-scala-scalatest from build
    
    * style: apply scalafmt formatting
    
    * style: apply scalafmt formatting

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,10 +2,11 @@ import sbt.*
 
 object Dependencies {
   private object Versions {
-    val avro         = "1.12.0"
-    val schReqClient = "8.0.0"
-    val scalatest    = "3.2.19"
-    val mockito      = "1.17.37"
+    val avro           = "1.12.0"
+    val schReqClient   = "8.0.0"
+    val scalatest      = "3.2.19"
+    val mockito        = "1.17.37"
+    val testcontainers = "1.19.3"
   }
 
   lazy val avroCompiler: ModuleID = "org.apache.avro" % "avro-compiler" % Versions.avro
@@ -15,4 +16,7 @@ object Dependencies {
 
   lazy val scalatest: ModuleID    = "org.scalatest" %% "scalatest"               % Versions.scalatest % Test
   lazy val mockitoScala: ModuleID = "org.mockito"   %% "mockito-scala-scalatest" % Versions.mockito   % Test
+
+  lazy val testcontainersKafka: ModuleID =
+    "org.testcontainers" % "kafka" % Versions.testcontainers % IntegrationTest
 }
